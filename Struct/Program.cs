6dbc3d0795bd393e8168c6d4c00f2da33cdb6ef4
Victoria Zan <victoriazan95@gmail.com>
2018-03-11 18:43:39 -0500

    ??

--- Struct/Program.cs
+++ Struct/Program.cs
@@ -10,20 +10,51 @@ namespace Struct
     {
         static void Main(string[] args)
         {
-            Point point;
-            point.x = 349;
-            point.y = 76;
-            point.Display();
-            point.Increment();
-            point.Display();
-            point.Decrement();
-            point.Display();
+            /* Point point;
+             point.x = 349;
+             point.y = 76;
+             point.Display();
+             point.Increment();
+             point.Display();
+             point.Decrement();
+             point.Display();
 
-            Point p1 = new Point();
-            p1.Display();
+             Point p1 = new Point();
+             p1.Display();
 
-            Point p2 = new Point(12, 22);
-            p2.Display();
+             Point p2 = new Point(12, 22);
+             p2.Display();*/
+
+            /* Point p1 = new Point(10, 20);
+             Point p2 = p1;
+             p1.Display();
+             p2.Display();
+             p1.Increment();
+             p2.Decrement();
+             p1.Display();
+             p2.Display();
+             //--------------------------------
+             PointRef pr1 = new PointRef(22, 44);
+             PointRef pr2 = pr1;
+             pr2.x = 77;
+             pr1.Display();
+             pr2.Display();*/
+            int? a = 100;
+            a = null;
+            char? c = null;
+
+            DataBaseReader dr = new DataBaseReader();
+
+            int? i = dr.GetInt()?? 100;
+            if(i.HasValue)
+                Console.WriteLine("Value of i is {0}",i.Value);
+            else
+                Console.WriteLine("i is undefined");
+            bool? b = dr.GetBool();
+            if(b!=null)
+                Console.WriteLine("value of b is {0}", b.Value);
+            else
+                Console.WriteLine("b is undefined");
         }
         struct Point
         {
@@ -49,4 +80,39 @@ namespace Struct
             }
         }
     }
+
+    class PointRef
+    {
+        public int x;
+        public int y;
+
+        public PointRef(int XPos, int YPos)
+        {
+            x = XPos;
+            y = YPos;
+        }
+        public void Increment()
+        {
+            x++; y++;
+        }
+        public void Decrement()
+        {
+            x--; y--;
+        }
+        public void Display()
+        {
+            Console.WriteLine("X={0}, Y={1},", x, y);
+        }
+    }
+    class DataBaseReader
+    {
+        public int? numericValue = null;
+        public bool? boolValue = true;
+
+        public int? GetInt()
+        { return numericValue; }
+        public bool? GetBool()
+        { return boolValue; }
+            
+     }
 }
