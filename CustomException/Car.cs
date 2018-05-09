401d91c6b7a438424741153606e4bf051bf595fe
Victoria Zan <victoriazan95@gmail.com>
2018-05-09 16:40:01 -0500

    customException

---
+++ CustomException/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CustomException
+{
+    class Car
+    {
+        public const int MaxSpeed = 100;
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        private bool carIsDead;
+        private Radio theMusicBox = new Radio();
+
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+        public void CrankTunes(bool state)
+        {
+            theMusicBox.TurnOn(state);
+        }
+        public void Accelerate(int delta)
+        {
+            if(carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if(CurrentSpeed >= MaxSpeed)
+                {
+                    
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                    CarIsDeadException ex = new CarIsDeadException(string.Format("{0} has overheated!!!", PetName), "speed", DateTime.Now);
+                    ex.HelpLink = "https://www.Help.com";
+                    ex.Data.Add("TimeStamp", string.Format("Time {0}", DateTime.Now));
+                    ex.Data.Add("Cause", string.Format("speed"));
+                    throw ex;
+                    
+                }
+                else
+                    Console.WriteLine("Current speed is {0}", CurrentSpeed);
+
+            }
+
+        }
+    }
+}
---
+++ CustomException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Exemple*****");
+            Console.WriteLine("Creating a car!!!");
+            Car myCar = new Car("Zippy", 20);
+            myCar.CrankTunes(true);
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                    myCar.Accelerate(10);
+            }
+            catch (CarIsDeadException ex)
+            {
+                Console.WriteLine("Message {0}, Time {1}, Cause {2}", ex.Message, ex.ErrorTimeStamp, ex.CauseOfError);
+                
+            }
+        }
+    }
+}
