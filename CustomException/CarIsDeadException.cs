e8b3876e429dbd7ef6e42e4a44fcaa8f3e46dda5
Victoria Zan <victoriazan95@gmail.com>
2018-05-09 16:53:47 -0500

    exc

--- CustomException/CarIsDeadException.cs
+++ CustomException/CarIsDeadException.cs
@@ -8,23 +8,17 @@ namespace CustomException
 {
     public class CarIsDeadException:ApplicationException
     {
-        private string messageDetails = String.Empty;
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
         public CarIsDeadException()
         {    }
         public CarIsDeadException(string message, string cause, DateTime time)
+            :base(message)
         {
-            messageDetails = message;
             CauseOfError = cause;
             ErrorTimeStamp = time;
         }
-        public override string Message
-        {
-            get
-            {
-                return string.Format("Car Error Message: {0}", messageDetails);
-            }
-        }
+        
     }
+
 }
